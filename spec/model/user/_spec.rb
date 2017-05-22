ecd8e7e7ecb8c0ae0c2eed3e5cf2b7b2b9dc0eb6
Christopher Fryman <christopher@fortyau.com>
2017-05-22 18:57:19 -0500

    first pass at adding rspec

---
+++ spec/model/user/_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe User, type: :model, user:true do
+
+  before(:each) do
+    # @model = FactoryGirl.create(:model)
+  end
+
+
+  it 'knows what you did last summer' do
+    expect(true).to be_true
+  end
+
+
+end
